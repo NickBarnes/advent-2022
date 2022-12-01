19b5003d9adeff0dbc85f6468e7de62bc194117c
Nick Barnes <Nick.Barnes@pobox.com>
2022-12-01 10:06:01 +0000

    God help me I can't stop tinkering with this

--- 01/go.py
+++ 01/go.py
@@ -3,7 +3,7 @@ import os
 
 def go(filename):
     print(f"results from {filename}:")
-    elves = [e.split() for e in open(filename,'r').read().split('\n\n')]
+    elves = [e.split() for e in open(filename, 'r').read().split('\n\n')]
     calories = sorted([sum(int(l) for l in e) for e in elves])
     print(f"  answer 1 (max calories of any elf): {calories[-1]}")
     print(f"  answer 2 (total calories of top three elves): {sum(calories[-3:])}")
