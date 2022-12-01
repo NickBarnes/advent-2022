f8bbc3650dd7425c906f229bb8fc3e701c4e3b1d
Nick Barnes <Nick.Barnes@pobox.com>
2022-12-01 09:28:34 +0000

    A slightly better framework for future days, plus a bit of spit and polish on my solution.

--- 01/go.py
+++ 01/go.py
@@ -1,11 +1,15 @@
 import sys
+import os
 
-input='input.txt'
+def go(filename):
+    print(f"results from {filename}:")
+    elves = [e.split() for e in open(filename,'r').read().split('\n\n')]
+    calories = sorted([sum(int(l) for l in e) for e in elves])
+    print(f"  answer 1 (max calories of any elf): {calories[-1]}")
+    print(f"  answer 2 (total calories of top three elves): {sum(calories[-3:])}")
+
 if len(sys.argv) > 1:
-    input = sys.argv[1]
-
-elves = open(input,'r').read().split('\n\n')
-calories = [sum(int(l) for l in e.split()) for e in elves]
-print(f"answer 1 (max calories of any elf): {max(calories)}")
-calories.sort()
-print(f"answer 2 (total calories of top three elves): {sum(calories[-3:])}")
+    for arg in sys.argv[1:]:
+        go(arg)
+else:
+    go(os.path.join(os.path.dirname(__file__), 'input.txt'))
