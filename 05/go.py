3832e76857c5a2e37c0c10d801234b0112fed0f7
Nick Barnes <nb@ravenbrook.com>
2022-12-05 11:39:52 +0000

    A bit of tidying up

--- 05/go.py
+++ 05/go.py
@@ -18,21 +18,29 @@ def go(filename):
                  for l in moves.split('\n') if l)
 
     # copy the crate stacks so we can manipulate them twice
-    crates_1 = [stack[:] for stack in crates]
+    crates_copy = [stack[:] for stack in crates]
+
+    # move N crates from stack a to stack b
+    def move(a,b,n):
+        crates[b-1] += crates[a-1][-n:]
+        crates[a-1] = crates[a-1][:-n]
+    
+    # report the set of top crates
+    def done(desc):
+        top_crates = ''.join(crate[-1] for crate in crates)
+        print(f"    {desc}: {top_crates}")
 
     # part 1: move one crate at at time
     for n,a,b in moves:
         for i in range(n):
-            crates_1[b-1].append(crates_1[a-1].pop())
-    top_crates = ''.join(crate[-1] for crate in crates_1)
-    print(f"    answer one (one crate at a time): {top_crates}")
+            move(a,b,1)
+    done("one crate at a time")
 
+    crates = crates_copy
     # part 2: move n crates at at time
     for n,a,b in moves:
-        crates[b-1] += crates[a-1][-n:]
-        crates[a-1] = crates[a-1][:-n]
-    top_crates = ''.join(crate[-1] for crate in crates)
-    print(f"    answer two (N crates at a time): {top_crates}")
+        move(a,b,n)
+    done("N crates at a time")
 
 # daily boilerplate for applying 'go' to files on the command-line or
 # to input.txt if there are none.
