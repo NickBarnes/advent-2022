abaa7003dbd179266767570feaf8cba07c539d64
Nick Barnes <nb@ravenbrook.com>
2022-12-10 09:58:31 +0000

    Slightly nicer template.

--- Makefile
+++ Makefile
@@ -6,3 +6,8 @@ day-%: %/go.py %/test*.txt %/input.txt
 	for f in $(wildcard $*/test*.txt); do python3 $*/go.py $$f; done
 	python3 $*/go.py $*/input.txt
 
+.PHONY: clean
+clean:
+	rm *~ */*~
+
+
--- template/go.py
+++ template/go.py
@@ -1,12 +1,18 @@
-# boilerplate for reading a file in different ways. Returns:
+# Boilerplate to support Advent of Code hackery
+
+import sys
+import os
+from run import run
+
+# Read a file in different ways and apply "run" to:
 # - a list of lists of the lines in each section (separated by blank lines)
 # - a list of the lines in the file
 # - a list of lists of words on each line
 # - a list of lists of digits on every line of a digit-grid file
 
-def read(filename):
+def go(filename):
     # sometimes we want sections, sometimes we want lines
-    sections = [[l.strip() for l in s.strip().split('\n') for s in open(filename,'r').read().split('\n\n')]
+    sections = [[l.strip() for l in s.strip().split('\n')] for s in open(filename,'r').read().split('\n\n')]
     lines = [l.strip() for l in open(filename,'r')]
 
     # often if we want lines we also want parts of each line
@@ -17,18 +23,10 @@ def read(filename):
         grid = [int(c) for l in lines for c in l]
     except:
         grid = [[]]
-    return sections, lines, words, grid
-    
-
-def go(filename):
-    print(f"results from {filename}:")
-    sections, lines, words, grid = read(filename)
+    run(filename, sections, lines, words, grid)
 
 # daily boilerplate for applying 'go' to files on the command-line or
 # to input.txt if there are none.
-
-import sys
-import os
 
 if len(sys.argv) > 1:
     for arg in sys.argv[1:]:
