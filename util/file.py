079ae852ed5b6c6fb7ce9cc7fd48f19b6e8b8f9d
Nick Barnes <nb@ravenbrook.com>
2022-12-12 12:07:48 +0000

    Rewrite day 12 to use utility grid-walking module.

--- util/file.py
+++ util/file.py
@@ -1,13 +1,12 @@
 # Boilerplate to support Advent of Code hackery
-
 import sys
 import os
 
-def files():
+def files(file = None):
     if len(sys.argv) > 1:
         return sys.argv[1:]
     else:
-        return os.path.join(os.path.dirname(__file__), 'input.txt')
+        return os.path.join(file, 'input.txt')
 
 def lines(filename):
     return [l.strip() for l in open(filename,'r') if l]
