7f0bf1e6de51c5113c97afbe263c4fbaeb2022c1
Nick Barnes <nb@ravenbrook.com>
2022-12-12 12:09:56 +0000

    Changed template to use util modules.

--- template/go.py
+++ template/go.py
@@ -1,14 +1,19 @@
-import boiler
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util'))
+
+import walk
+import file
 
 def go(filename):
     print(f"results from {filename}:")
     # pick one:
-    # sections = boiler.sections(filename)
-    # lines = boiler.lines(filename)
-    # words = boiler.words(filename)
-    # digits = boiler.digits(filename)
-    # chars = boiler.chars(filename)
+    # sections = file.sections(filename)
+    # lines = file.lines(filename)
+    # words = file.words(filename)
+    # digits = file.digits(filename)
+    # chars = file.chars(filename)
 
 if __name__ == '__main__':
-    for f in boiler.files():
+    for f in file.files(__file__):
         go(f)
