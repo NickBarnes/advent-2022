a538cd800ff237c047ee75a98c0b0fcf25eacddb
Nick Barnes <nick@tarides.com>
2023-01-09 17:47:59 +0000

    Much better day 16 part 2, which I've been itching to write for weeks.

--- 16/go.py
+++ 16/go.py
@@ -29,69 +29,42 @@ class Valve:
 
 # what's the best score if I arrive at valve `pos` to open it, with
 # remaining time `t` and `to_open` other valves left to open, and what
-# (reversed route) do I follow to get it?
+# (reversed) route do I follow to get it?
+
+cache = {}
 
 def best(pos, t, to_open):
-    if t <= 0: # no time left
-        return 0,[]
-    score = pos.flow * (t-1)
+    key = (pos, t, frozenset(to_open))
+    if key in cache:
+        return cache[key]
+    score = pos.flow * (t-1) # from opening this valve
     time_left = t-1 if pos.flow else t
-    if to_open:
-        next_best,route = max(best(v, time_left-pos.distances[v], to_open-{v}) for v in to_open)
-    else:
-        next_best,route = (0,[])
-    route.append((pos, t))
-    return (score + next_best, route)
+    next_best,route = max((best(v, nt, to_open-{v})
+                           for v in to_open
+                           if (nt := time_left-pos.distances[v]) > 0),
+                          default=(0,[]))
+    route = route[:] + [(pos, t)]
+    score += next_best
+    cache[key] = (score, route)
+    return score, route
 
-# what's the best score we can produce if I arrive at `pos1` with time
-# `t1` left, to open the valve there, and my pachyderm assistant
-# arrives at `pos2` with time `t2` left, to open the valve there, with
-# `to_open` *other* valves left to open. And what (reversed) routes do
-# we each follow to get it?
+# With the assistance of Tramples, Crusher of Enemies: maximise over
+# all partitions of the valves into two sets (one for me, one for
+# Tramples). Could doubtless do better than this but this is fairly
+# quick.
 
-def best2(pos1, t1, pos2, t2, to_open):
-    if t1 > t2: # I arrive first to open my valve
-        if 0 >= t1: # no time left
-            return 0,[],[]
-        score = pos1.flow * (t1-1)
-        t1left = t1-1 if pos1.flow else t1
-        if to_open: # more valves to open
-            next_best, route1, route2 = max(best2(v, t1left-pos1.distances[v], pos2, t2, to_open-{v}) for v in to_open)
-        else:
-            next_best, route1, route2 = best2(None, 0, pos2, t2, set())
-        route1.append((pos1, t1))
-        return score + next_best, route1, route2
-    elif t1 < t2: 
-        next_best, route1, route2 = best2(pos2, t2, pos1, t1, to_open)
-        return next_best, route2, route1
-    else: # t1 == t2
-        if 0 >= t1:
-            return 0,[],[]
-        if pos1 == pos2: # both at the same valve at the same time: only one of us opens it
-            score = pos1.flow * (t1-1)
-            t1left = t1-1 if pos1.flow else t1
-            t2left = t2
-        else: # we open two different valves in this minute
-            score = (pos1.flow + pos2.flow) * (t1-1)
-            t1left = t1-1 if pos1.flow else t1
-            t2left = t2-1 if pos2.flow else t2
-        if len(to_open) > 1:
-            next_best, route1, route2 = max(max(best2(v1, t1left-pos1.distances[v1],
-                                                      v2, t2left-pos2.distances[v2],
-                                                      to_open-{v1} - {v2}) for v2 in to_open - {v1})
-                                            for v1 in to_open)
-        elif len(to_open) == 1: # race to the last valve
-            v = to_open.pop()
-            next_best, route1, route2 = best2(v, t1left-pos1.distances[v], None, 0, set())
-            next_best_2, route1_2, route2_2 = best2(None, 0, v, t2left-pos2.distances[v], set())
-            if next_best_2 > next_best:
-                next_best, route1, route2 = next_best_2, route1_2, route2_2
-        else: # no valves left
-            next_best, route1, route2 = 0, [], []
-        route1.append((pos1, t1))
-        if pos1 != pos2:
-            route2.append((pos2, t2))
-        return score + next_best, route1, route2
+import itertools
+
+def best2(pos, t, closed):
+    max, br1, br2 = 0, None, None
+    for r in range(len(closed) // 2):
+        for c in itertools.combinations(closed, r):
+            h = frozenset(c)
+            p1, r1 = best(pos, t, h)
+            p2, r2 = best(pos, t, closed - h)
+            if p1 + p2 > max:
+                max, br1, br2 = p1 + p2, r1, r2
+    return max, br1, br2
 
 valve_re = re.compile("Valve (.*) has flow rate=(.*); tunnels? leads? to valves? (.*)")
 
@@ -106,14 +79,16 @@ def go(filename):
         v.dist()
     
     score, route = best(valves['AA'],30, set(v for v in vs if v.flow))
-    print('\n'.join(f"  I open {v.id} in minute {30-t}, scoring {(t-1)*v.flow}" for v,t in route[::-1] if v.flow))
+    print('\n'.join(f"  I open {v.id} in minute {30-t}, scoring {(t-1)*v.flow}"
+                    for v,t in route[::-1] if v.flow))
     print(f"Total pressure released in 30 minutes (answer one): {score}")
-
-    score, route1, route2 = best2(valves['AA'],26,valves['AA'],26,set(v for v in vs if v.flow))
+    
+    score, route1, route2 = best2(valves['AA'], 26, frozenset(v for v in vs if v.flow))
     r = [(t,v,n) for r,n in [(route1, "I open"),(route2, "Tramples opens")] for v,t in r]
     r.sort()
-    print('\n'.join(f" {n} valve {v.id} in minute {26-t}, scoring {(t-1)*v.flow}" for t,v,n in r if v.flow))
-    print(f"total pressure released in 26 minutes with the help of Tramples the Crushera (answer two): {score}")
+    print('\n'.join(f" {n} valve {v.id} in minute {26-t}, scoring {(t-1)*v.flow}"
+                    for t,v,n in r if v.flow))
+    print(f"total pressure released in 26 minutes with the help of Tramples, Crusher of Enemies (answer two): {score}")
 
 if __name__ == '__main__':
     for f in file.files(__file__):
